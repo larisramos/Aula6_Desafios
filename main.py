7c70b8b02ddeb368492d814e43caed226511b373
Larissa Ramos <68797820+larisramos@users.noreply.github.com>
2022-03-08 11:57:07 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,4 @@
+'''Faça um programa que leia algo pelo teclado e mostre na tela o seu tipo primitivo e todas as informações possiveis sobre ele'''
+
+n1 = input("Digite uma letra ou número: \n")
+print() #tem que usar o isa alpha ou numeric, etc
